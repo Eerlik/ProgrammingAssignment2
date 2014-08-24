17a0a2f608f6e7e9e2f1b4c49fad1c2c6be00cdf
Eerlik <erik.redelinghuys@gmail.com>
2014-08-24 10:51:02 +0200

    First commit to initialise
    Basically just checking whether the file is committing correctly.

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Two functions to cache the inverse of any invertible matrix
 
-## Write a short comment describing this function
-
+## fun1:makeCacheMatrix creates a special matrix object that can cache its inverse 
 makeCacheMatrix <- function(x = matrix()) {
 
+  #Initialise the inverse property as empty
+  I <- NULL
+  
+  #Function/method to set the matrix
+  set <- function(y) {
+    x <<- matrix
+    I <<- NULL
+  }
+  
+  
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+  
 }
 
 
@@ -12,4 +27,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+  
 }
